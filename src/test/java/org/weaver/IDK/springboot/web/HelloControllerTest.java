2b026b05cfad1b7f13cc2eb07d6cb3072d41a358
weaver9651 <weaver9651@gmail.com>
2020-12-21 23:56:36 +0900

    p110까지 완료

--- src/test/java/org/weaver/IDK/springboot/web/HelloControllerTest.java
+++ src/test/java/org/weaver/IDK/springboot/web/HelloControllerTest.java
@@ -1,10 +1,8 @@
 package org.weaver.IDK.springboot.web;
 
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
