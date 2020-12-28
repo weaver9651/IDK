4bca1665db751240ae8f5766072ece265bdb6385
weaver9651 <weaver9651@gmail.com>
2020-12-29 01:33:09 +0900

    p123까지 완료

--- src/test/java/org/weaver/IDK/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/org/weaver/IDK/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,17 +1,16 @@
 package org.weaver.IDK.springboot.domain.posts;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -44,5 +43,28 @@ public class PostsRepositoryTest {
 		Posts posts = postsList.get(0);
 		assertThat(posts.getTitle()).isEqualTo(title);
 		assertThat(posts.getContent()).isEqualTo(content);
+	}
+
+	@Test
+	public void register_BaseTimeEntity() {
+		// given
+		LocalDateTime now = LocalDateTime.of(2020, 12, 29, 0, 0, 0);
+		postsRepository.save(Posts.builder()
+		.title("title")
+		.content("content")
+		.author("author")
+		.build());
+
+		// when
+		List<Posts> postsList = postsRepository.findAll();
+
+		// then
+		Posts posts = postsList.get(0);
+
+		System.out.println(">>>>>>>>>>>>>>>>> createdDate=" + posts.getCreatedDate()
+		+ ", modifiedDate=" + posts.getModifiedDate());
+
+		assertThat(posts.getCreatedDate()).isAfter(now);
+		assertThat(posts.getModifiedDate()).isAfter(now);
 	}
 }
